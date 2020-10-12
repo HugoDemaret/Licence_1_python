72a9f1bec0fda1e9a94ea34caa2e2929e7b156d1
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2020-10-12 16:40:05 +0200

    Update TD3_Exo5.py

--- TD3_Exo5.py
+++ TD3_Exo5.py
@@ -1,8 +1,10 @@
+#Demaret Hugo
+
 init_number = int(input("Entrez le premier nombre"))
 card = 2
 sum = 0
-while init_number !=0:
-    sum += init_number
-    init_number = int(input("Entrez le " +str(card) + "nombre"))
+while init_number !=0: #tant que le nombre rentré n'est pas 0
+    sum += init_number #fait la somme avec les nombres précédent
+    init_number = int(input("Entrez le " +str(card) + "nombre")) #rentre le n-ième nombre
     card += 1
-print("La somme des entiers saisis vaut:", sum)
+print("La somme des entiers saisis vaut:", sum) #affiche la somme totale
