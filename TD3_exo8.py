98ad229f2ce76f8c3701f1ae8409beea8ab5d305
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2020-10-12 16:38:46 +0200

    Update TD3_exo8.py

--- TD3_exo8.py
+++ TD3_exo8.py
@@ -1,7 +1,12 @@
-rice_mass = float(758000000000)
-n = float(2**63)
-i = 1
-while rice_mass < ((n)*0.0025):
+#Demaret Hugo
+
+rice_mass = float(758000000000) #masse de riz en gramme
+n = float(2**63) #nombre total de grain de riz
+i = 1 #initialisation à 1
+while rice_mass < ((n)*0.0025): #tant que la production annuelle de grain de riz n'atteint pas la masse totale
     i +=1
-    rice_mass += 758000000000
-print(i)
+    rice_mass += 758000000000 #on additionne l'équivalent de chaque année
+print(i) #on affiche le nombre d'années nécéssaires
+
+
+#Note importante: on peut résoudre ce problème à l'aide d'une simple division.
