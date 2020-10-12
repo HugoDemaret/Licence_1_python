73c726433626be64bb1e171630fa4ee9cec5d761
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2020-10-12 22:19:10 +0200

    Update Suite_Collatz_Exemple.py

--- Defi_de_la_semaine/Suite_Collatz_Exemple.py
+++ Defi_de_la_semaine/Suite_Collatz_Exemple.py
@@ -12,15 +12,17 @@
 #ainsi que les diverses approches utilisées, toutes plus intéressantes et élégantes les unes que les autres!
 
 #Voici le code de cet exemple:
-#Algorithme de calcul de la suite de Collatz en fonction d'une seed rentrée par l'utilisateur
+#Algorithme de calcul de la suite de Collatz en fonction d'une seed rentrée par l'utilisateur:
+
 integer_1 = int(input("Rentrez la valeur de la graine"))
 i = 0
 while integer_1 != 1:
     i += 1
-    if integer_1 % 2 == 0:
-        integer_1 = integer_1 // 2
-    else:
-        integer_1 = (3 * integer_1) + 1
+    if integer_1 % 2 == 0:                                  #Test la parité de Un ( "a%b" nous donne le reste de la division euclidienne de a par b)
+        integer_1 = integer_1 // 2                          #Un+1 = Un/2, attention ici à bien mettre un "//" afin d'avoir une division entière !
+    else:                                                   #Si un nombre entier n'est pas pair, il est impair, donc on peut mettre une condition else
+        integer_1 = (3 * integer_1) + 1                     #Un+1 = 3Un+1
     print("Le terme " + str(i) + " de la suite est égal à " + str(integer_1))
 print("Votre graine a un temps de vol de " + str(i))
+
 #Note: on pourrait optimiser cet algorithme pour qu'il élimine d'entrée de jeu les puissances de 2
