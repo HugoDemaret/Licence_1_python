cd7f8155dcf45e8d29073254794accd422b6ba71
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2020-11-14 16:21:46 +0100

    Create Defi_2_Correction.py

---
+++ Defi_de_la_semaine/Anciens sujets/Defi_2_Correction.py
@@ -0,0 +1,42 @@
+#Voici la correction du programme python du défi 2! 
+
+def convert(liste):
+    liste2 = []
+    for i in liste:             #parcourt la liste afin de traiter chaque string qu'elle contient
+        if i == '':
+            print("Erreur: votre liste contient une chaine vide!\n")
+            break
+        else:
+            sum = ''
+            for j in i:
+                number = str(ord(j)) #décompose la string en char, prend la valeur ASCII du char, et fait une concaténation avec les char précédents
+                sum += str(number)
+            liste2.append(int(sum))
+            print(liste2)
+    return liste2
+
+def quicksort(liste):
+    if len(liste) < 1:
+        return liste
+    else:
+        pivot = liste.pop() #selectionne le pivot
+
+    greater_than = []
+    lower_than = []
+
+    for item in liste:
+        if item > pivot:
+            greater_than.append(item)
+
+        else:
+            lower_than.append(item)
+
+    return quicksort(lower_than) + [pivot] + quicksort(greater_than) #récursivité de traitement
+
+
+def main():
+    liste = ['1', 'a', 'chat', 'Chiens', 'arbre', '47884abed', 'fr-FR', '5.1', '7', '9', '-15', '157', '011001', '3', '3', 'b', 'a', 'EC2EAC']
+    liste2 = convert(liste)
+    print("QuickSort: ", quicksort(liste2))
+
+main()
